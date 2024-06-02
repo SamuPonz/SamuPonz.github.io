b6979c38f8f1ec5d3578bafd84e644c7b3cb9aa6
Samuele Ponzin <38397791+samuponz@users.noreply.github.com>
2024-06-02 11:46:23 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
